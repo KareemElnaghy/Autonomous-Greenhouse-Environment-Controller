0d997c436513220156c5d347ecc24d9c317ca0ea
Kareem Elnaghy <kareemelnaghy@aucegypt.edu>
2026-05-12 23:24:50 +0300

    removed light gpio pin

--- Src/main.c
+++ Src/main.c
@@ -603,7 +603,7 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_WritePin(GPIOA, GPIO_Fertilizer_Pin|GPIO_Water_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, GPIO_Light_Pin|LD3_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, LD3_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pins : GPIO_Fertilizer_Pin GPIO_Water_Pin */
   GPIO_InitStruct.Pin = GPIO_Fertilizer_Pin|GPIO_Water_Pin;
@@ -612,8 +612,8 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : GPIO_Light_Pin LD3_Pin */
-  GPIO_InitStruct.Pin = GPIO_Light_Pin|LD3_Pin;
+  /*Configure GPIO pins :  LD3_Pin */
+  GPIO_InitStruct.Pin = LD3_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
